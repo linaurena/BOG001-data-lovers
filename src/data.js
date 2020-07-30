8626689ff29e6a243c056a56c5328df1305cbce7
danimtbr <damantillab@gmail.com>
2020-07-30 11:15:58 -0500

    Envio la función de filtro, dificultad, y ascendente

--- src/data.js
+++ src/data.js
@@ -1,9 +1,45 @@
-// estas funciones son de ejemplo
+import data from "./data/lol/lol.js";
+const datos = Object.values(data.data);
+let championsDiv = document.getElementById("root");
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+for (const champion of datos) {
+    root.innerHTML += `
+    <div>
+      <h3>${champion.name}</h3>
+      <img class = 'imgchampion' src = '${champion.splash}' />
+    </div>
+    `
+  }
+
+
+  // Función de filtro//
+
+
+  const filterData = (data,condition)=>{
+    const role_one = data.map((dato) => [dato.name,dato.tags[0],dato.splash]); 
+    const role_two = data.map((dato) => [dato.name,dato.tags[1],dato.splash]);   
+    const classify = role_one.filter(role => role[1] == condition);
+    const classify_two = role_two.filter(role => role[1] == condition);
+    const final = classify.concat(classify_two);
+    return final;
+  };
+  //console.log(filterData(datos,'Assassin'));//
+
+  //Función Dificultad//
+
+  const filterDif = (data, num) =>{
+    const difficulty = data.map((dato) => [dato.name,dato.info.difficulty,dato.splash]);
+    const diff = difficulty.filter(dif => dif[1] == num);
+    return diff;
+  }; 
+ // console.log(filterDif(datos,1))//
+
+  // Función Sort//
+
+  const organize = (data) => {
+    let dataorga = data.map((dato)=> [dato.name,dato.splash]);
+    return dataorga.sort();
+  };
+  console.log(organize(datos))
+  
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import data from "./data/lol/lol.js";
 const datos = Object.values(data.data);
-const championsDiv = document.getElementById("root");
+let championsDiv = document.getElementById("root");
 
 
 for (const champion of datos) {
@@ -11,11 +11,66 @@ for (const champion of datos) {
     </div>
     `
   }
+
+document.getElementById("bot").addEventListener("click",NewElement);
   
-  const nombres = datos.map((dato) => dato.name); //map va dato por dato y ejecuta una función y lo pone en un arreglo nuevo//
-  console.log(nombres);
+
+
+  // const difficult = datos.map((dato) => dato.lenght > 6);//
+  //console.log(difficult);
+
+//function classify () {
+  //const tags = ['Tank', 'Mage', 'Assasin', 'Controller', 'Fighter', 'Slayer', 'Marksman'];
+  //const roles = classify.length;
+  //const mago= datos.filter(tags == 'Mage');
+//}
+
+  //const nombres = datos.map((dato) => dato.name); //map va dato por dato y ejecuta una función y lo pone en un arreglo nuevo//
+  //console.log(nombres);
   
+  //const difficult = datos.filter((dato) => dato.info.difficulty > 6);
+  //console.log(difficult);
+
+  /*
+  const tags = ['Tank', 'Mage', 'Assasin', 'Controller', 'Fighter', 'Slayer', 'Marksman'];
+
+  const result = datos.filter(tags == 'Mage');
+*/
+//console.log(classify);
+
+/*
+let role = ['Tank', 'Mage', 'Assasin', 'Controller', 'Fighter', 'Slayer', 'Marksman'];
+role.sort();
+console.log(role);
+
+/*
+
+newProductArray => []
+{id: 1, tag = 'mage'}
+{id: 2, tag = 'tank'}[]
+newProductArray = productArray.filter((champion, tag) => tag => 1)
+/*
+  const role = newArray (7);
+
+  fighter[0];
+  tank [1];
+  mage [2];
+  support [3];
+  assasin [4];
+  specialist [5];
+  controller [6];
+
+   const role = datos.map((dato) => dato.role[0]);
+
+/*
+  const role = datos.filter((dato) => dato.tags[0]);
+  console.log(role);
+*/
+
+  /*
   const difficult = datos.filter((dato) => dato.info.difficulty > 6);
   console.log(difficult);
+*/
+
   
-  
+  //1. //Aplicar Filtro//
