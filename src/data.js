e0d37369ff6464f9c54098d87352e4e2b0c6650a
danimtbr <damantillab@gmail.com>
2020-08-03 12:08:25 -0500

    actualizacion del proyecto

--- src/data.js
+++ src/data.js
@@ -1,17 +1,3 @@
-import data from "./data/lol/lol.js";
-const datos = Object.values(data.data);
-let championsDiv = document.getElementById("root");
-
-
-for (const champion of datos) {
-    root.innerHTML += `
-    <div>
-      <h3>${champion.name}</h3>
-      <img class = 'imgchampion' src = '${champion.splash}' />
-    </div>
-    `
-  }
-
 
   // Función de filtro//
 
